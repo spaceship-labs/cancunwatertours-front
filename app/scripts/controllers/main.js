163603b3acc0fb280c3a064636b386c0041b583a
tugorez <tugorez@undefined>
2016-06-01 13:48:47 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cancunwatertoursFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cancunwatertoursFrontApp
+ */
+angular.module('cancunwatertoursFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
